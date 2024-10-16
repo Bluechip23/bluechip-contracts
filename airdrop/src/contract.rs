a23e0cddb0f29a326c719b3568b47be8aa001c09
nightfury1111111111 <linjay0208@gmail.com>
2024-10-16 10:11:23 +0200

    update cosmwasm std version

--- airdrop/src/contract.rs
+++ airdrop/src/contract.rs
@@ -1,7 +1,11 @@
+use std::env;
+
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    Addr, AllBalanceResponse, BankMsg, BankQuery, Coin, CosmosMsg, QuerierWrapper, QueryRequest,
+    Addr, AllBalanceResponse, BankMsg, BankQuery, Coin, CosmosMsg, DepsMut, Env, MessageInfo,
+    QuerierWrapper, QueryRequest, Response, StdResult, Uint128,
 };
-use cosmwasm_std::{DepsMut, Env, MessageInfo, Response, StdResult, Uint128};
 use cw2::set_contract_version;
 // use cw_storage_plus::{Item, Map};
 // use serde::{Deserialize, Serialize};
@@ -13,6 +17,7 @@ use crate::state::{State, CLAIMED, STATE, WHITELISTED};
 const CONTRACT_NAME: &str = "crates.io:airdrop";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
@@ -34,6 +39,7 @@ pub fn instantiate(
     Ok(Response::new().add_attribute("method", "instantiate"))
 }
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
     env: Env,
--- airdrop/src/msg.rs
+++ airdrop/src/msg.rs
@@ -1,18 +1,16 @@
+use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Uint128};
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
 // use crate::state::Recipient;
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[cw_serde]
 pub struct InstantiateMsg {
     pub total_whitelist_wallets: Uint128,
     pub eligible_wallets: Uint128,
     pub airdrop_amount: Uint128,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub enum ExecuteMsg {
     // SetRewards { recipients: Vec<Recipient> },
     ImportWhitelist { whitelist: Vec<Addr> },
