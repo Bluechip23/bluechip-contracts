fc83ed01bd51ec1b437b7ec4eb503095fb0c1b31
nick <nickdavisbc@gmail.com>
2025-12-08 01:49:53 +0000

    deleted uneccesary scripts

--- debug_fee_state.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-POOL_ADDR="cosmos1g3cgv3z2smd7vrjr3s639ewnwyd5dz4lg0uy9fadhkumca4suqcqzrdgdu"
-
-echo "🔍 Querying Pool State..."
-bluechipChaind query wasm contract-state smart $POOL_ADDR '{"pool_state":{}}'
-
-echo "🔍 Querying Fee State..."
-bluechipChaind query wasm contract-state smart $POOL_ADDR '{"fee_state":{}}'
